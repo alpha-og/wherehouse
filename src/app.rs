b5cede896b57a32a35cfa8b991ae1a1a86ee491a
Athul Anoop <athulanoop2020@gmail.com>
2025-05-27 22:37:03 +0530

    feat: panes for search bar, status bar and list

--- src/app.rs
+++ src/app.rs
@@ -1,34 +1,40 @@
 use color_eyre::eyre::Context;
 use crossterm::event::{self, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
+    layout::{Constraint, Layout},
     style::Stylize,
     symbols::border,
     text::{Line, Text},
-    widgets::{Block, Paragraph},
+    widgets::{Block, List, Paragraph},
 };
 
 use crate::tui;
 
+enum Pane {
+    Search,
+    List,
+}
+
+enum InputMode {
+    Normal,
+    Insert,
+}
+
 pub struct App {
+    current_pane: Pane,
+    search_query: String,
+    input_mode: InputMode,
     quit: bool,
 }
 
 impl Default for App {
     fn default() -> Self {
-        Self { quit: false }
-    }
-}
-
-impl ratatui::widgets::Widget for &App {
-    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
-    where
-        Self: Sized,
-    {
-        let title = Line::from(" WhereHouse ".bold());
-        Block::bordered()
-            .title(title.centered())
-            .border_set(border::THICK)
-            .render(area, buf);
+        Self {
+            current_pane: Pane::Search,
+            search_query: String::new(),
+            input_mode: InputMode::Normal,
+            quit: false,
+        }
     }
 }
 
@@ -42,7 +48,25 @@ impl App {
     }
 
     pub fn draw(&self, frame: &mut ratatui::Frame) {
-        frame.render_widget(self, frame.area());
+        let splits = Layout::vertical([
+            Constraint::Length(3),
+            Constraint::Min(1),
+            Constraint::Length(3),
+        ])
+        .split(frame.area());
+
+        let search_bar = Block::bordered()
+            .title("Search".bold())
+            .title_alignment(ratatui::layout::Alignment::Left);
+        frame.render_widget(search_bar, splits[0]);
+
+        let list = List::new(vec!["hello", "world", "ratatui"]).block(Block::bordered());
+        frame.render_widget(list, splits[1]);
+
+        let status_bar = Paragraph::new("WhereHouse 0.1.0")
+            .alignment(ratatui::layout::Alignment::Right)
+            .block(Block::bordered());
+        frame.render_widget(status_bar, splits[2]);
     }
 
     fn handle_events(&mut self) -> color_eyre::Result<()> {
@@ -57,6 +81,7 @@ impl App {
     fn handle_key_event(&mut self, key_event: KeyEvent) -> color_eyre::Result<()> {
         match key_event.code {
             KeyCode::Char('q') => self.quit(),
+
             _ => {}
         }
         Ok(())
