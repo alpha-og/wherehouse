b1968c3e824e35a082a385b8a946fc1ecd349483
Athul Anoop <athulanoop2020@gmail.com>
2025-05-30 21:57:29 +0530

    update: switch to shared state

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,8 @@
 use std::{
-    fmt::Display,
     process::Command,
-    sync::mpsc::{self, Sender},
-    thread,
-    time::Duration,
+    sync::{Arc, mpsc::Sender},
 };
 
-use color_eyre::{eyre::Context, owo_colors::OwoColorize};
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEvent, KeyEventKind},
     layout::{Constraint, Layout},
@@ -17,27 +13,23 @@ use ratatui::{
 };
 
 use crate::{
-    state::{self, Pane, State, StateEvent, StateItemType, StateResponse},
+    state::{self, Pane, State},
     tui,
 };
 
 pub struct Tui {
-    tx_state: Sender<StateEvent>,
+    state: State,
 }
 
 impl Tui {
-    pub fn new(tx_state: Sender<StateEvent>) -> Self {
-        Self { tx_state }
+    pub fn new(state: Arc<State>) -> Self {
+        Self { state }
     }
     pub fn run(&mut self, terminal: &mut tui::Tui) -> color_eyre::Result<()> {
         loop {
-            if State::sync_tui(&self.tx_state) {
-                terminal.draw(|frame| self.draw(frame))?;
-            };
-            if let Some(StateResponse::ShouldQuit(should_quit)) =
-                State::get(&self.tx_state, state::StateItemType::ShouldQuit)
-            {
-                if should_quit {
+            terminal.draw(|frame| self.draw(frame))?;
+            if let Ok(should_quit) = self.state.should_quit.try_lock() {
+                if *should_quit {
                     break;
                 }
             }
@@ -57,10 +49,8 @@ impl Tui {
         let mut search_bar_style = Style::default();
         let mut list_style = Style::default();
 
-        if let Some(StateResponse::CurrentPane(current_pane)) =
-            State::get(&self.tx_state, StateItemType::CurrentPane)
-        {
-            match current_pane {
+        if let Ok(current_pane) = self.state.current_pane.lock() {
+            match *current_pane {
                 Pane::SearchInput => search_bar_style = active_style,
                 Pane::SearchResults => list_style = active_style,
                 _ => {}
@@ -71,35 +61,24 @@ impl Tui {
             .border_type(BorderType::Rounded)
             .title("Search".bold())
             .title_alignment(ratatui::layout::Alignment::Left);
-        if let Some(StateResponse::SearchQuery(search_query)) =
-            State::get(&self.tx_state, StateItemType::SearchQuery)
-        {
-            let search_input = Paragraph::new(search_query)
+        if let Ok(search) = self.state.search.lock() {
+            let search_input = Paragraph::new(search.query.clone())
                 .left_aligned()
                 .block(search_bar)
                 .style(search_bar_style);
             frame.render_widget(search_input, splits[0]);
-        }
 
-        let active_list_item = Style::new().bold().bg(Color::White).fg(Color::Black);
-
-        if let Some(StateResponse::SearchResults(Some(search_results))) =
-            State::get(&self.tx_state, StateItemType::SearchResults)
-        {
-            if let Some(StateResponse::SelectedSearchResult(selected_search_result)) =
-                State::get(&self.tx_state, StateItemType::SearchSelectedResult)
-            {
-                let list = List::new(search_results.iter().enumerate().map(|(i, item)| {
-                    if i == selected_search_result {
-                        ListItem::new(item.display_text.clone()).style(active_list_item)
-                    } else {
-                        ListItem::new(item.display_text.clone()).style(Style::default())
-                    }
-                }))
-                .block(Block::bordered().border_type(BorderType::Rounded))
-                .style(list_style);
-                frame.render_widget(list, splits[1]);
-            }
+            let active_list_item = Style::new().bold().bg(Color::White).fg(Color::Black);
+            let list = List::new(search.results.iter().enumerate().map(|(i, item)| {
+                if i == search.selected_result {
+                    ListItem::new(item.display_text.clone()).style(active_list_item)
+                } else {
+                    ListItem::new(item.display_text.clone()).style(Style::default())
+                }
+            }))
+            .block(Block::bordered().border_type(BorderType::Rounded))
+            .style(list_style);
+            frame.render_widget(list, splits[1]);
         } else {
             let list = List::new(vec![""])
                 .block(Block::bordered().border_type(BorderType::Rounded))
@@ -121,9 +100,7 @@ impl Tui {
 
         let status_bar_splits =
             Layout::horizontal([Constraint::Percentage(70), Constraint::Fill(1)]).split(splits[2]);
-        if let Some(StateResponse::InputMode(input_mode)) =
-            State::get(&self.tx_state, StateItemType::InputMode)
-        {
+        if let Ok(input_mode) = self.state.input_mode.lock() {
             let status_bar_left = Span::styled(
                 format!(" {} ", input_mode),
                 Style::default()
