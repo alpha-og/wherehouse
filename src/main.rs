d3be90a02d1518f3a581266e705c7809374f880a
Athul Anoop <athulanoop2020@gmail.com>
2025-06-02 23:00:55 +0530

    chore: refactored and reorganized tui related code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,31 @@
 use input::InputHandler;
 use logging::initialize_logging;
 use state::State;
-use std::{
-    sync::{Arc, mpsc},
-    thread,
-};
-use worker::{Worker, WorkerEvent};
+use std::{sync::Arc, thread};
+use task_manager::TaskManager;
+use tracing::info;
 
-mod app;
+mod commands;
 mod input;
 mod logging;
 mod state;
+mod task_manager;
 mod tui;
 mod widget;
-mod worker;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     initialize_logging()?;
-    trace_dbg!("initialized logging");
+    info!("initialized logging");
     let state = Arc::new(State::new());
-    let (tx_worker, rx_worker) = mpsc::channel::<WorkerEvent>();
+    let task_manager = TaskManager::new(state.clone());
 
-    let mut worker = Worker::new(rx_worker, state.clone());
-    let _worker_thread = thread::spawn(move || worker.run());
-    trace_dbg!("Worker thread initiated");
-
-    let mut input_handler = InputHandler::new(state.clone(), tx_worker);
+    let mut input_handler = InputHandler::new(state.clone(), task_manager);
     let _input_thread = thread::spawn(move || input_handler.run());
-    trace_dbg!("Input handler thread initiated");
+    info!("Input handler thread initiated");
 
     let mut terminal = tui::init()?;
-    let tui = app::Tui::new(state).run(&mut terminal);
+    let tui = tui::Tui::new(state).run(&mut terminal);
     if let Err(err) = tui::restore() {
         eprintln!("failed to restore terminal {err}");
     }
--- src/tui.rs
+++ src/tui.rs
@@ -1,3 +1,15 @@
+use std::sync::Arc;
+
+use ratatui::layout::{Constraint, Layout};
+
+use crate::{
+    state::State,
+    widget::{
+        info_pane::InfoPane, search_input_pane::SearchInputPane,
+        search_results_pane::SearchResultsPane, status_bar::StatusBar,
+    },
+};
+
 use ratatui::{
     Terminal,
     crossterm::{
@@ -8,9 +20,7 @@ use ratatui::{
 };
 use std::io;
 
-pub type Tui = Terminal<CrosstermBackend<io::Stdout>>;
-
-pub fn init() -> io::Result<Tui> {
+pub fn init() -> io::Result<Terminal<CrosstermBackend<io::Stdout>>> {
     execute!(io::stdout(), EnterAlternateScreen)?;
     enable_raw_mode()?;
     set_panic_hook();
@@ -29,4 +39,51 @@ pub fn restore() -> io::Result<()> {
     execute!(io::stdout(), LeaveAlternateScreen)?;
     disable_raw_mode()?;
     Ok(())
+}
+
+pub struct Tui {
+    state: Arc<State>,
+}
+
+impl Tui {
+    pub fn new(state: Arc<State>) -> Self {
+        Self { state }
+    }
+    pub fn run(
+        &mut self,
+        terminal: &mut Terminal<CrosstermBackend<io::Stdout>>,
+    ) -> color_eyre::Result<()> {
+        loop {
+            terminal.draw(|frame| self.draw(frame))?;
+            if let Ok(should_quit) = self.state.should_quit.try_lock() {
+                if *should_quit {
+                    break;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub fn draw(&self, frame: &mut ratatui::Frame) {
+        let layout =
+            Layout::vertical(vec![Constraint::Fill(1), Constraint::Length(1)]).split(frame.area());
+
+        let main_layout = Layout::horizontal(vec![Constraint::Percentage(40), Constraint::Fill(1)])
+            .split(layout[0]);
+
+        let search_layout = Layout::vertical(vec![Constraint::Length(3), Constraint::Fill(1)])
+            .split(main_layout[0]);
+
+        let search_input_pane = SearchInputPane::new(self.state.clone());
+        frame.render_widget(search_input_pane, search_layout[0]);
+
+        let search_results_pane = SearchResultsPane::new(self.state.clone());
+        frame.render_widget(search_results_pane, search_layout[1]);
+
+        let info_pane = InfoPane::new(self.state.clone());
+        frame.render_widget(info_pane, main_layout[1]);
+
+        let status_bar = StatusBar::new(self.state.clone());
+        frame.render_widget(status_bar, layout[1]);
+    }
 }
