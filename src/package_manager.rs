756fb2075e8d18b6a6a9fb829b585d892f780981
Athul Anoop <athulanoop2020@gmail.com>
2025-06-04 19:01:49 +0530

    update: moved command and spawn_command functions to common module

--- src/package_manager.rs
+++ src/package_manager.rs
@@ -1,7 +1,49 @@
+use std::{
+    ffi::OsStr,
+    process::{Command, Stdio},
+};
+
 mod homebrew;
 
 pub type SpawnCommandResult = Result<std::process::Child, std::io::Error>;
 pub type CommandResult = std::io::Result<std::process::Output>;
+
+/// spawn a non-blocking command returning the Child
+pub fn spawn_command<I, S>(args: I) -> SpawnCommandResult
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    Command::new("brew")
+        .args(args)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+}
+
+/// create a blocking command and run until completion returning the output wrapped in a Result
+pub fn command<I, S>(args: I) -> CommandResult
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    Command::new("brew").args(args).output()
+}
+
+pub enum PackageLocality {
+    Local,
+    Remote,
+}
+
+trait PackageManager {
+    fn filter_packages(source: PackageLocality, pattern: String) -> Result<Vec<String>, String>;
+    fn check_health() -> Result<String, String>;
+    fn clean() -> Result<String, String>;
+    fn package_info(package_name: String) -> Result<String, String>;
+    fn install(package_name: String) -> Result<(), String>;
+    fn upgrade(package_name: String) -> Result<(), String>;
+    fn uninstall(package_name: String) -> Result<(), String>;
+}
 
 // enum PackageManager {
 //     Homebrew,
@@ -24,4 +66,3 @@ pub type CommandResult = std::io::Result<std::process::Output>;
 // }
 //
 //
-
